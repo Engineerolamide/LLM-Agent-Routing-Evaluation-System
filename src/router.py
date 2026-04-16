94bb4be1eb8e6b4ffa25bdcc0223ec6d85a3eb7f
Engineerolamide <olamidehgold@gmail.com>
2026-04-16 16:54:04 -0500

    Update router.py

--- src/router.py
+++ src/router.py
@@ -1,1 +1,7 @@
-
+def route_input(category):
+    if category == "motivational":
+        return "motivational_agent"
+    elif category == "advisory":
+        return "advisory_agent"
+    else:
+        return "informational_agent"
